36242e151e962d39302658910972e572b00f28f0
Sidhartha Amperayani <ga5310@g.rit.edu>
2022-07-05 12:57:03 -0400

    Time: 92 ms (31.46%), Space: 13.9 MB (91.25%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -9,7 +9,7 @@ class Solution:
         for row in range(rows):
             for col in range(cols):
                 if grid[row][col] == 1: fresh += 1
-                if grid[row][col] == 2: q.append([row, col])
+                if grid[row][col] == 2: q.append((row, col))
             
         while q and fresh > 0:
             directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
@@ -20,7 +20,7 @@ class Solution:
                     if r in range(rows) and c in range(cols) and grid[r][c] == 1:
                         grid[r][c] = 2
                         fresh -= 1
-                        q.append([r, c])
+                        q.append((r, c))
             time += 1
         return time if fresh == 0 else -1
                 
