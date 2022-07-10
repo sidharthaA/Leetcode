2f3ae5ff55a23420c39a6f3a29fdeca0d15c552f
Sidhartha Amperayani <ga5310@g.rit.edu>
2022-07-09 23:27:44 -0400

    Time: 73 ms (5.65%), Space: 13.9 MB (71.19%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def repeat(nums):
+            rob1, rob2 = 0, 0
+            for n in nums:
+                rob2, rob1 = max(n + rob1, rob2), rob2
+            return rob2
+        if len(nums) > 1:
+            val1 = repeat(nums[0:len(nums) - 1])
+            val2 = repeat(nums[1:len(nums)])
+            return max(val1, val2)
+        else:
+            return repeat(nums)
+        
