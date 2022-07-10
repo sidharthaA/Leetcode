acc4970545d57c8099ddf0b24491125caba25523
Sidhartha Amperayani <ga5310@g.rit.edu>
2022-07-09 23:08:08 -0400

    Time: 62 ms (17.72%), Space: 13.8 MB (65.73%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,11 +1,11 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        dp = [i for i in nums]
-        # print(dp)
-        for i in range(len(nums) - 3, -1, -1):
-            # print("i = ", i)
-            for j in range(i + 2, len(nums)):
-                # print("j = ", j)
-                dp[i] = max(dp[i], nums[i] + dp[j])
-                # print("dp[i] = ", dp[i])
-        return max(dp)
+        # dp = [i for i in nums]
+        # for i in range(len(nums) - 3, -1, -1):
+        #     for j in range(i + 2, len(nums)):
+        #         dp[i] = max(dp[i], nums[i] + dp[j])
+        # return max(dp)
+        rob1, rob2 = 0, 0
+        for n in nums:
+            rob2, rob1 = max(n + rob1, rob2), rob2
+        return rob2
