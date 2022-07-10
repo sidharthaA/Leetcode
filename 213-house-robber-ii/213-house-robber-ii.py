9c82cb35a1e57d0a999c89a8afcfa3775546a155
Sidhartha Amperayani <ga5310@g.rit.edu>
2022-07-09 23:35:07 -0400

    Time: 47 ms (56.87%), Space: 13.9 MB (24.41%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -5,6 +5,6 @@ class Solution:
             for n in nums:
                 rob2, rob1 = max(n + rob1, rob2), rob2
             return rob2
-        val1 = repeat(nums[0:len(nums) - 1])
-        val2 = repeat(nums[1:len(nums)])
+        val1 = repeat(nums[:-1])
+        val2 = repeat(nums[1:])
         return max(nums[0], val1, val2)
