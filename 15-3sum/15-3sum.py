a4bc70f36637e3dddf268fdd4f878fe690208c3b
Sidhartha Amperayani <ga5310@g.rit.edu>
2022-07-20 16:51:38 -0400

    Time: 1362 ms (49.27%), Space: 18.2 MB (41.74%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,22 +1,38 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        nums.sort()
-        
-        for i, a in enumerate(nums):
-            if i > 0 and a == nums[i - 1]:
+        nums = sorted(nums)
+        output = []
+        # print(nums)
+        for k in range(len(nums)):
+            if k > 0 and nums[k] == nums[k - 1]:
                 continue
-            
-            l, r = i + 1, len(nums) - 1
-            while l < r:
-                threeSum = a + nums[l] + nums[r]
-                if threeSum > 0:
-                    r -= 1
-                elif threeSum < 0:
-                    l += 1
+            i, j = k + 1, len(nums) - 1
+            target = 0 - nums[k]
+            while i < j:
+                if nums[i] + nums[j] < target:
+                    i += 1
+                elif nums[i] + nums[j] > target:
+                    j -= 1
                 else:
-                    res.append([a, nums[l], nums[r]])
-                    l += 1
-                    while nums[l] == nums[l - 1] and l < r:
-                        l += 1
-        return res
+                    output.append([nums[i], nums[j], nums[k]])
+                    i += 1
+                    while nums[i] == nums[i - 1] and i < j:
+                        i += 1
+        return output
+
+        # for i, a in enumerate(nums):
+        #     if i > 0 and a == nums[i - 1]:
+        #         continue
+        #     l, r = i + 1, len(nums) - 1
+        #     while l < r:
+        #         threeSum = a + nums[l] + nums[r]
+        #         if threeSum > 0:
+        #             r -= 1
+        #         elif threeSum < 0:
+        #             l += 1
+        #         else:
+        #             res.append([a, nums[l], nums[r]])
+        #             l += 1
+        #             while nums[l] == nums[l - 1] and l < r:
+        #                 l += 1
+        # return res
