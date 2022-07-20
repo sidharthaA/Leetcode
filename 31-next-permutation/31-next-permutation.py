67e24453cc2966240a953b1cee5d2fb4a812dadd
Sidhartha Amperayani <ga5310@g.rit.edu>
2022-07-20 18:57:21 -0400

    Time: 46 ms (89.26%), Space: 13.9 MB (24.65%) - LeetHub

--- 31-next-permutation/31-next-permutation.py
+++ 31-next-permutation/31-next-permutation.py
@@ -6,23 +6,16 @@ class Solution:
         pivot = 0
         n = nums
         for i in range(len(n) -1, 0, -1):
-            # print(i)
             if n[i] <= n[i - 1]:
                 continue
             else:
                 pivot = i
                 break
-        # print("pivot=",pivot)
         if pivot == 0:
             nums = n.sort()
-        # print(n)
         for i in range(len(n) - 1, pivot - 1, -1):
-            # print("inside", n[i])
             if n[pivot - 1] < n[i]:
-                # print("deep inside",n[pivot - 1], n[i], "pivot-1=", pivot-1)
                 n[pivot - 1], n[i] = n[i], n[pivot - 1]
                 break
-        # print(n)
         n[pivot: len(n)] = sorted(n[pivot: len(n)])
-        # print(n)
         nums = n
