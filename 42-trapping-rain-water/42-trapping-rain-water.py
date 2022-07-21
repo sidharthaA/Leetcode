f657b5536b3359d30057666bb6e70e4b7ef77244
Sidhartha Amperayani <ga5310@g.rit.edu>
2022-07-20 21:13:50 -0400

    Time: 204 ms (44.91%), Space: 16.2 MB (9.08%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -1,41 +1,99 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        heights = height
-        # start = 0
-        # end = 1
-        # count = 0
-        # while end < len(height):
-        #     count1 = 0
-        #     if height[end] < height[start]:
-        #         x = max(height[end:])
-        #         if x > height[start]:
-        #             count1 = count1 + height[start] - height[end]
-        #         else:
-        #             if end != len(height) - 1:
-        #                 count1 = count1 + height[start] - height[end] -(height[start] - x)
-        #         if end < len(height) - 1:
-        #             end = end + 1
-        #         else:
-        #             break
-        #     else:
-        #         count = count + count1
-        #         start = end
-        #         end = end + 1
-        # return count
+        left = []
+        right = []
+        final = []
+        maxVal = 0
+        for l in height:
+            left.append(maxVal)
+            if l > maxVal:
+                maxVal = l
+        maxVal = 0
+        for r in range(len(height) - 1, -1, -1):
+            right.append(maxVal)
+            temp = min(left[r], maxVal) - height[r]
+            if temp >= 0:
+                final.append(temp)
+            else:
+                final.append(0)
+            if height[r] > maxVal:
+                maxVal = height[r]
+        # print(left)
+        # print(right)
+        # print(sum(final))
+        return sum(final)
         
-        res = 0
-        max_left = max_right = 0        
-        i, j = 0, len(heights) - 1
-        while i <= j:
-            if max_left <= max_right: # add left
-                amount = max_left - heights[i]
-                res = res + amount if amount > 0 else res
-                max_left = max(max_left, heights[i])
-                i += 1
-            else: # add right
-                amount = max_right - heights[j]
-                res = res + amount if amount > 0 else res
-                max_right = max(max_right, heights[j])
-                j -= 1
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         heights = height
+#         # start = 0
+#         # end = 1
+#         # count = 0
+#         # while end < len(height):
+#         #     count1 = 0
+#         #     if height[end] < height[start]:
+#         #         x = max(height[end:])
+#         #         if x > height[start]:
+#         #             count1 = count1 + height[start] - height[end]
+#         #         else:
+#         #             if end != len(height) - 1:
+#         #                 count1 = count1 + height[start] - height[end] -(height[start] - x)
+#         #         if end < len(height) - 1:
+#         #             end = end + 1
+#         #         else:
+#         #             break
+#         #     else:
+#         #         count = count + count1
+#         #         start = end
+#         #         end = end + 1
+#         # return count
+        
+#         res = 0
+#         max_left = max_right = 0        
+#         i, j = 0, len(heights) - 1
+#         while i <= j:
+#             if max_left <= max_right: # add left
+#                 amount = max_left - heights[i]
+#                 res = res + amount if amount > 0 else res
+#                 max_left = max(max_left, heights[i])
+#                 i += 1
+#             else: # add right
+#                 amount = max_right - heights[j]
+#                 res = res + amount if amount > 0 else res
+#                 max_right = max(max_right, heights[j])
+#                 j -= 1
     
-        return res
+#         return res
