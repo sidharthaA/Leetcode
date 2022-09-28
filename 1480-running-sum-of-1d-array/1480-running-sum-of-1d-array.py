23682c393c22e4e8a4b26e9b7ab0adf600481ce6
Sidhartha Amperayani <ga5310@g.rit.edu>
2022-09-28 19:02:42 -0400

    Time: 83 ms (25.44%), Space: 14.2 MB (27.19%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -1,12 +1,12 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        # result = []
-        # a = 0
-        # for i in nums:
-        #     result.append(a + i)
-        #     a += i
-        # return result
+        result = []
+        a = 0
+        for i in nums:
+            result.append(a + i)
+            a += i
+        return result
         # replace nums starting from 2nd element with the sum of current and previous element
-        for i in range(1, len(nums)):
-            nums[i] += nums[i - 1]
-        return nums
+        # for i in range(1, len(nums)):
+        #     nums[i] += nums[i - 1]
+        # return nums
