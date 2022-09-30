9a85c351823561c5f880bbe22835ca6055cc873c
Sidhartha Amperayani <ga5310@g.rit.edu>
2022-09-29 21:46:00 -0400

    Time: 62 ms (34.39%), Space: 14.4 MB (5.78%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        # dictionary to track the positions of each character
+        count_dictionary = {}
+        for index, character in enumerate(t):
+            if character in count_dictionary:
+                count_dictionary[character].append(index)
+            else: 
+                count_dictionary[character] = [index]
+        print(count_dictionary)
+            
+        # Check each character in the dictionary if the position values are incrementing
+        current = float('-inf') # to keep a track of current position
+        flag = True
+        for character in s:
+            # print(character)
+            if character not in count_dictionary:
+                return False
+            for position in count_dictionary[character]:
+                if position > current:
+                    # print('position:', position, '\ncurrent:', current)
+                    current = position
+                    flag = False
+                    break
+            if flag == True:
+                return False
+            flag = True
+        return True
+            
+            
