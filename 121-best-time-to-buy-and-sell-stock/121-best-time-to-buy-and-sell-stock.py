5d1399400e9cbb4585dd353ade3fa5d108268ba8
Sidhartha Amperayani <ga5310@g.rit.edu>
2022-10-06 08:24:35 +0530

    Time: 2865 ms (9.93%), Space: 25.1 MB (7.00%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,15 +1,23 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        start = 0
-        end = 1
-        maxProfit = 0
-        profit = 0
+        '''Brute Force'''
+        # maximum = 0
+        # for i in range(len(prices) - 1):
+        #     for j in range(i + 1, len(prices)):
+        #         if prices[j] - prices[i] > maximum:
+        #             maximum = prices[j] - prices[i]
+        # return maximum
+        
+        '''O(n)'''
+        profit, max_profit = 0, 0
+        start, end = 0, 1
         while end < len(prices):
             if prices[end] < prices[start]:
                 start = end
-                end = end + 1
-            else:
-                profit = prices[end] - prices[start]
-                end = end + 1
-            maxProfit = max(profit, maxProfit)
-        return maxProfit   
+                end += 1
+                continue
+            profit = prices[end] - prices[start]
+            if max_profit < profit:
+                max_profit = profit
+            end += 1
+        return max_profit
