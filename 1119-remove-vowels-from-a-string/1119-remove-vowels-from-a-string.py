e68d7f7a559f816075a42f0bb4fedc6f39e25421
Sidhartha Amperayani <ga5310@g.rit.edu>
2022-11-21 03:16:53 -0500

    Time: 53 ms (58.13%), Space: 13.8 MB (95.59%) - LeetHub

---
+++ 1119-remove-vowels-from-a-string/1119-remove-vowels-from-a-string.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeVowels(self, s: str) -> str:
+        vowels = ['a', 'e', 'i', 'o', 'u']
+        output = ''
+        for i in s:
+            if i not in vowels:
+                output += i
+        return output
