7e726d3d6ffd3791bfd60f421b1f3d8582dea738
Sidhartha Amperayani <ga5310@g.rit.edu>
2025-06-14 11:46:49 -0700

    Time: 0 ms (100%), Space: 19.4 MB (5.96%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,21 @@
 class Solution:
+    # def twoSum(self, nums: List[int], target: int) -> List[int]:
+    #     tracker = {}
+    #     for i, num in enumerate(nums):
+    #         if num not in tracker:
+    #             tracker[target - num] = i
+    #         else:
+    #             return [i, tracker[num]]
+
+    # '''
+    # Time Complexity: O(n) — single pass through the list.
+    # Space Complexity: O(n) — for the hash map.
+    # '''
+
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        map = {}
-        for pos, num in enumerate(nums):
-            if num in map:
-                return [map[num], pos]
-            map[target - num] = pos
-        
+        complimentary = {}
+        for i, num in enumerate(nums):
+            if num in complimentary:
+                return [i, complimentary[num]]
+            complimentary[target - num] = i
+        return []
